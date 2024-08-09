5f34c7c13d25323e1980b353b60c4f34d666c371
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-08-09 20:43:00 +0530

    Ep3

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+// JSX => Babel transpile it ReactCreateElement => React(Object) => HtmlElement
+const Jsxhead = (
+  <h1 className="" tabIndex={1}>
+    Hello World
+  </h1>
+);
+// JSX in multiple lines- () helps babel to undertasnd where the code starts annd where it's ends
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "head" }, "Hello mai aaya"),
-    React.createElement("h2", { id: "head2" }, "Hello mai aaya 2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "head" }, "Hello mai aaya"),
-    React.createElement("h2", { id: "head2" }, "Hello mai aaya 2"),
-  ]),
-]);
+const Jsxheadmulti = (
+  <h1 className="" tabIndex={1}>
+    Hello World
+  </h1>
+);
+const HeadCom = function()  {
+  
+  return <h1>Hello World HeadCom</h1>;
+  <Head/>
+};
+// Component composition- calling other components in other components
+// loop of copoents just for fun😂😂😂
+const Head = () => {
+  return (
+    <div>
+      {funcon()}
+      <h1 id="heading">Hello World Head</h1>
+    </div>
+  );
+};
+const funcon = function() {
+  return (
+    <div>
+      <h1 id="heading">Hello World Funcon</h1>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Head />);
+
+// ReactDOM.render(React.createElement('h1', { id: 'heading' }, 'Hello World'), document.getElementById('root'));
