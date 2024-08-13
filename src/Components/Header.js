30fdaf9a491306b00bb7ed0f72735b485b0e7c8b
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-08-14 01:00:04 +0530

    Ep5

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+
+
+
+const Header = () => {
+    return (
+      <div className="header ">
+        <img src="assests/1600w-4rQv_oY-CF8.webp" alt="logo" />
+        <div className="lists">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  export default Header;
