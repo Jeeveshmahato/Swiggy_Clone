4487736873109b1a8c280c3d6b08f063fca4fa00
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-08-17 20:48:54 +0530

    EP6

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,34 +1,78 @@
-
 import Card from "./Card";
 import { restaurantList } from "../utils/restaurantList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
   // this is array is structuring
-  const [firstList, setfirstList] = useState(restaurantList);
+  const [firstList, setfirstList] = useState([]);
+  const [copyList, setCopyList] = useState([]);
+  const [SearchText, setsearchText] = useState("");
 
   // const arr  =useState(restaurantList);
   // const  [firstList, setfirstList]  = arr;
   // const firstList = arr[0];
   // const setfirstList = arr[1];
   const bih = () => {
-    let firstt = firstList.filter((res) => res.data.avgRating >= 4);
-    setfirstList(firstt);
+    let firstt = firstList.filter((res) => res.info.avgRating >= 4);
+    setCopyList(firstt);
   };
-  return (
+  const fecthdat = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.8045665&lng=86.2028754&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setfirstList( json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    setCopyList( json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    console.log(setfirstList( json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants));
+  };
+  useEffect(() => {
+    fecthdat();
+  }, []);
+  // conditional rendring
+  // if (firstList.length === 0) {
+  //   return <h1>Loading...</h1>;
+  // }
+
+  return firstList.length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
     <div className="body">
       <h1>Welcome to our website</h1>
       <p>This resturant is all your's now.💕</p>
+      <div className="search-box">
+        <input
+          type="search"
+          placeholder="Search..."
+          value={SearchText}
+          onChange={(e) => {
+            setsearchText(e.target.value);
+          }}
+          className="search"
+        ></input>
+        <button
+          onClick={() => {
+            const filtered = firstList.filter((res) =>
+              res.info.name.toLowerCase().includes(SearchText.toLowerCase())
+            );
+            setCopyList(filtered);
+          }}
+          className="search-btn"
+        >
+          Search
+        </button>
+      </div>
       <button onClick={bih} className="best-btn">
         Best options
       </button>
       <input type="text" className=""></input>
       <div className="cards">
-        {firstList.map((restaurant) => (
-          <Card key={restaurant.data.id} data={restaurant.data} />
+        {copyList.map((res) => (
+          <Card key={res.info.id} data={res.info} />
         ))}
       </div>
     </div>
   );
 };
 export default Body;
-
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <div className="header ">
-        <img src="assests/1600w-4rQv_oY-CF8.webp" alt="logo" />
-        <div className="lists">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+  const [BtnName, setBtnName] = useState("Login");
+  return (
+    <div className="header ">
+      <img src="assests/1600w-4rQv_oY-CF8.webp" alt="logo" />
+      <div className="lists">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <button
+            className="login"
+            onClick={() => {
+              BtnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {BtnName}
+          </button>
+        </ul>
       </div>
-    );
-  };
-  export default Header;
+    </div>
+  );
+};
+export default Header;
