2cfc41500500a28aa4ce81cc8f850b606bd803b6
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-08-20 18:56:19 +0530

    EP8 -Part2

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const USer = (prpos) => {
-  let [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(2)
+  let [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(2);
+  useEffect(() => {
+    const timmer = setInterval(() => {
+      console.log("timer");
+    }, 1000);
+    return () => {
+      clearInterval(timmer);
+    };
+  }, []);
+
   return (
     <div>
       <h1>{prpos.name}</h1>
       <p>This is {count}</p>
-      <button onClick={()=>{
-        setCount(count +  1);
-      }}>click kr</button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        click kr
+      </button>
       <p>This is {count2}</p>
     </div>
   );
