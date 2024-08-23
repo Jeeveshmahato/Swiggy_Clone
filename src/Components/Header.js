f2055b73e6fc71f07b8843dd40a9f1b1549549e4
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-08-23 11:44:00 +0530

    EP-10

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [BtnName, setBtnName] = useState("Login");
   return (
-    <div className="header ">
-      <img src="assests/1600w-4rQv_oY-CF8.webp" alt="logo" />
+    <div className=" flex justify-between items-center  bg-orange-400">
+      <img className=" w-[100px]" src="https://graphicsfamily.com/wp-content/uploads/edd/2021/07/Restaurant-Logo-Design-Template-scaled.jpg" alt="logo" />
       <div className="lists">
-        <ul>
+        <ul className="flex gap-8 items-center">
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>
           <li><Link to='/contactus'>Contact</Link></li>
