4f74c0b5a59b952567d3011c8f1c5fc3fd808b85
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-08-31 00:12:49 +0530

    EP-11-1

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,12 +6,26 @@ const Card = ({ data }) => {
   // const { data } = props;
   return (
     <div className="card w-fit px-6 py-4">
-      <img className="w-48 h-[100px]  self-stretch" src={imgLink + data.cloudinaryImageId} alt="card" />
+      <img
+        className="w-48 h-[100px]  self-stretch"
+        src={imgLink + data.cloudinaryImageId}
+        alt="card"
+      />
       <h2>{data.name}</h2>
       <p>{data.avgRating}</p>
 
       <button>{data.costForTwo}</button>
     </div>
   );
+};
+export const RatCard = (Card) => {
+  return (props) => {
+    return (
+      <div>
+        <label>Rated</label>
+        <Card {...props} />
+      </div>
+    );
+  };
 };
 export default Card;
